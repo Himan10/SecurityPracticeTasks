8cac2951d9304c10d0dcaf803bca5b3ab5b853d9
Himan10 <h3c.initiative46@gmail.com>
2023-01-13 02:11:56 +0530

    replaced integer from chars in stdout

--- sha1OfAlphabeticalLetters.py
+++ sha1OfAlphabeticalLetters.py
@@ -1,19 +1,30 @@
 # Create sha1 of possible combinations from AAAA to ZZZZ
 # sha1 has been cryptographically broken and there are many replacements of it. 
 
+# Here, due to recursionLimit and SegmentationFault, I've restricted it to only 4 chars, from 'A' to 'D'
+# Will fix soon
+
+import hashlib
+import sys
+
+def printSHA1ofChars(data):
+    alphabets = ''.join(chr(i) for i in data)
+    getSha = hashlib.sha1(alphabets.encode())
+    print(alphabets, getSha.hexdigest())
+
 def getCombinations(array, index):
     if index > -1: 
         if index == len(array)-1:
-            while array[index] != 3:
+            while array[index] != 68:
                 array[index] += 1
-                print(array)
+                printSHA1ofChars(array)
             else:
-                array[index] = 0
+                array[index] = 64
                 getCombinations(array, index-1)
         else:
             array[index] += 1
-            if array[index] > 3:
-                array[index] = 0
+            if array[index] > 68:
+                array[index] = 64
                 getCombinations(array, index-1)
             else:
                 getCombinations(array, index+1)
@@ -21,6 +32,6 @@ def getCombinations(array, index):
     return
 
 def main():
-    data = [1,1,1]
-    print(data)
+    data = [65, 65, 65, 65]
+    printSHA1ofChars(data)
     getCombinations(data, len(data)-1)
