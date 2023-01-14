a4d01a5d99fe1805c62a6f319e3e06aa50e02bff
Himan10 <h3c.initiative46@gmail.com>
2023-01-14 19:52:58 +0530

    Fixed the recursion issue in this, implemented iteration approach

--- sha1OfAlphabeticalLetters.py
+++ sha1OfAlphabeticalLetters.py
@@ -1,18 +1,19 @@
 # Create sha1 of possible combinations from AAAA to ZZZZ
 # sha1 has been cryptographically broken and there are many replacements of it. 
 
-# Here, due to recursionLimit and SegmentationFault, I've restricted it to only 4 chars, from 'A' to 'D'
-# Will fix soon
-
 import hashlib
 import sys
 
-def printSHA1ofChars(data):
+def printSHA1ofChars(data, fp):
     alphabets = ''.join(chr(i) for i in data)
     getSha = hashlib.sha1(alphabets.encode())
+    if fp:
+        fp.write(f"{alphabets}, {getSha.hexdigest()}\n")
+        return
     print(alphabets, getSha.hexdigest())
 
 def getCombinations(array, index):
+    # This uses recursion but it can lead to MaxRecursionDepth Error or segmentation fault, although it works fine.
     if index > -1: 
         if index == len(array)-1:
             while array[index] != 68:
@@ -31,7 +32,19 @@ def getCombinations(array, index):
 
     return
 
+def getCombinationsAndPrintSHA():
+    # Better function
+    with open("output.txt", "w") as file:
+        for i in range(65, 91):
+            for j in range(65, 91):
+                for k in range(65, 91):
+                    for l in range(65, 91):
+                        printSHA1ofChars([i, j, k, l], file)
+
+    print("Output has been written to \"output.txt\"")
+
 def main():
     data = [65, 65, 65, 65]
-    printSHA1ofChars(data)
-    getCombinations(data, len(data)-1)
+    #printSHA1ofChars(data)
+    #getCombinations(data, len(data)-1)
+    getCombinationsAndPrintSHA()
